9564815964bf9d2b92b58f20bcc7573a4ca7c426
Pavel Demichev <pasha.demichev@gmail.com>
2021-04-07 22:04:08 +1200

    initial commit

---
+++ ftp_sync.ps1
@@ -0,0 +1,30 @@
+$uri = "ftp://172.18.148.101/"
+$uri = "ftp://10.10.0.103/foobar2000 Music Folder"
+$localFileName = "%d0%90%d0%ba%d0%b2%d0%b0%d1%80%d0%b8%d1%83%d0%bc-%d0%a1%d0%b5%d1%81%d1%82%d1%80%d0%b0 %d0%a5%d0%b0%d0%be%d1%81 [2002, %d0%a1%d0%be%d1%8e%d0%b7, SZCD 1429-02]-%d0%91%d1%80%d0%b0%d1%82 %d0%9d%d0%b8%d0%ba%d0%be%d1%82%d0%b8%d0%bd.mp3"
+$fileName = "ftp://10.10.0.103/foobar2000 Music Folder/%d0%90%d0%ba%d0%b2%d0%b0%d1%80%d0%b8%d1%83%d0%bc-%d0%a1%d0%b5%d1%81%d1%82%d1%80%d0%b0 %d0%a5%d0%b0%d0%be%d1%81 [2002, %d0%a1%d0%be%d1%8e%d0%b7, SZCD 1429-02]-%d0%91%d1%80%d0%b0%d1%82 %d0%9d%d0%b8%d0%ba%d0%be%d1%82%d0%b8%d0%bd.mp3"
+function get-ftpdir {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+
+    $ftpWebRequest = [System.Net.FtpWebRequest]::Create($uri)
+    $ftpWebRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails
+    
+    $ftpWebRequest.UseBinary = $true
+    $ftpCreds = New-Object -TypeName System.Net.NetworkCredential
+    $ftpCreds.UserName = "anonymous"
+    
+    $ftpWebRequest.Credentials = $ftpCreds
+
+    $ftpResponse = $ftpWebRequest.GetResponse()
+
+    $responseStream = $ftpResponse.GetResponseStream()
+    
+    $encoding = [System.Text.Encoding]::GetEncoding(URL)
+    $streamReader = New-Object -TypeName System.IO.StreamReader -ArgumentList $responseStream, $encoding, $true
+    
+    return $streamReader.ReadToEnd()
+
+}
